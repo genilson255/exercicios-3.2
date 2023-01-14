c032aa0e2c532b9fd2f3df318c8d6e9d36bc2029
Genilson <genilson255@hotmail.com>
2023-01-13 21:48:56 -0300

    Add Promise<TUser[]>

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,10 +2,13 @@ import {RowDataPacket} from 'mysql2/promise';
 import {TUser} from '../types';
 import connection from './connection';
 
-const getAll = async () => {
+
+// RowDataPacket = usado quando fizer um SELECT
+// ResulSetHeader = usado quando fizer um INSERT, DELETE OU UPDATE// okPacket = set
+const getAll = async (): Promise<TUser[]> => {
   // O password dessa function na aula nao foi colocado
-   const [rows] = await connection.execute<>,RowDataPacket('SELECT id, name, email, password from users;');
-   return rows as TUser[];
+   const [rows] = await connection.execute<RowDataPacket[] & TUser[]> ('SELECT id, name, email, password from users;');
+   return rows;
 
 }
 export default {getAll};
