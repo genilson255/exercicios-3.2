b51059124ec27a0c5f92cb5c34004140ef411c9b
Genilson <genilson255@hotmail.com>
2023-01-13 21:37:16 -0300

    Add dependencias do projecto

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,7 +1,10 @@
+import {RowDataPacket} from 'mysql2/promise';
+import {TUser} from '../types';
 import connection from './connection';
 
 const getAll = async () => {
-   const [rows] await connection.execute('SELECT id, name, email, password from users;');
+  // O password dessa function na aula nao foi colocado
+   const [rows] = await connection.execute<>,RowDataPacket('SELECT id, name, email, password from users;');
    return rows as TUser[];
 
 }
