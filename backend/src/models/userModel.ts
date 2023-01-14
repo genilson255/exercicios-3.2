fa12679705590eaca15b2e6698f959988ac02826
Genilson <genilson255@hotmail.com>
2023-01-13 21:19:47 -0300

    Add tipagem ao userModel.ts

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,7 +2,7 @@ import connection from './connection';
 
 const getAll = async () => {
    const [rows] await connection.execute('SELECT id, name, email, password from users;');
-   return rows;
+   return rows as TUser[];
 
 }
 export default {getAll};
