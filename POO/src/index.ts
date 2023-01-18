b837970819d47818d47aabb76eac954db97a82fa
genilson <genilson255@hotmail.com>
2023-01-17 21:57:41 -0300

    Add progreso com erro de lint

--- POO/src/index.ts
+++ POO/src/index.ts
@@ -1,18 +1,37 @@
 import Conta from "./conta";
+// teste 1
+// const minhaConta = new Conta(1000);
+// try {
+//   //minhaConta.debito(1);
+// } catch (error) {
+//   console.log(String(error));
+  
+// }
 
+// try {
+//   minhaConta.credito(1000);  
+// } catch (error) {
+//   console.log("Moeda valida, Credito efetuado com sucesso");
+  
+// }
+
+// console.log(minhaConta.saldo);
+
+// Teste 2
 const minhaConta = new Conta(1000);
-try {
-  //minhaConta.debito(1);
-} catch (error) {
-  console.log(String(error));
-  
-}
+const amanha = new Date();
+amanha.setDate(amanha.getDate() + 1);
+// console.log(amanha);
 
-try {
-  minhaConta.credito(1000);  
-} catch (error) {
-  console.log("Moeda valida, Credito efetuado com sucesso");
-  
-}
+const pagamento = new pagamento({
+  conta: minhaConta, 
+  valor: 100, 
+  tipoPagamento: 'boleto', 
+  vencimento: amanha, 
+  descricao: 'Conta da claro';
+});
 
+pagamento.pagar();
 console.log(minhaConta.saldo);
+
+
